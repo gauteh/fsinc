12d978baff8e03d459243893ad45d5e60ac48032
Gaute Hope <gaute.hope@met.no>
2020-05-27 09:42:16 +0200

    package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fsinc',
+    version='0.1.0',
+    url='https://github.com/gauteh/fsinc.git',
+    author='Gaute Hope',
+    author_email='gauteh@met.no',
+    description='Implementation of fast sinc-transform based on https://github.com/hannahlawrence/sinctransform',
+    packages=find_packages(),
+    install_requires=['numpy', 'numba'],
+)
+
